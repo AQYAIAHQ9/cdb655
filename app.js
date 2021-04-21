fc58518b45b3caeb3a6ac713e3e335f3c536a901
AQYAIAHQ9 <70981697+AQYAIAHQ9@users.noreply.github.com>
2021-04-21 01:40:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//* App de Teste Do Sistema
+//* IP: cdn.gamerworldgg.ga
+//* HTML, JS, CSS
+const { cdn } = require('payload-3');
+const IP.config = {
+    "cdn":".get-ip"
+}
+
+const server.app = require('host');
+console.log('app rodando');
+//console 1.0 do Servidor
+//txt: cdn.txt
+
+const cdn = new(server, payloads() =>{
+    if cdn.event.off = return()
+    //evento 001
+    if html.down = console.log('html crash encontrado');
+    //evento 002
+    if cdn.crash = new('reload\n');
+    //no crash app
+    if ram.down = return ('reload, refresh'( =>{
+        %crash%:()
+        if crash = value(0);
+        console.log('');
+        .patch();
+        ["json-type","reload"]
+        //* no crash cdn App
+    })
+});
+
+
+//* App JavaScript
+//* HTML, CSS, JS
+//* ^ 5.9^,^
